8699e84ad5154355d810190df87ec9797ef4958e
iliapapela <iliapapela@mail.ru>
2024-05-26 23:14:35 +0300

    n3 try

--- src/zadanie_3.java
+++ src/zadanie_3.java
@@ -3,6 +3,11 @@ public class zadanie_3 {
         printColor();
     }
     public static void printColor() {
+    int value = 7;
+    if (value <= 0);
+        System.out.println("Красный");
+    else (value == 0 - 100); //если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”
+        System.out.println("Желтый");
 
     }
 }
