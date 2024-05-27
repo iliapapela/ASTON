ed2819b7ab85b19ce18d757534a1692f5d615f72
iliapapela <iliapapela@mail.ru>
2024-05-27 21:34:27 +0300

    3 done

--- src/zadanie_3.java
+++ src/zadanie_3.java
@@ -2,12 +2,16 @@ public class zadanie_3 {
     public static void main(String[] args) {
         printColor();
     }
-    public static void printColor() {
-    int value = 7;
-    if (value <= 0);
-        System.out.println("Красный");
-    else (value == 0 - 100); //если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”
-        System.out.println("Желтый");
 
+    public static void printColor() {
+        int value = 110;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
     }
 }
