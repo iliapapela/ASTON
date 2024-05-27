bea386a93bec492287e14269652bcc0628b78443
iliapapela <iliapapela@mail.ru>
2024-05-27 21:55:47 +0300

    4 done

---
+++ src/zadanie_4.java
@@ -0,0 +1,15 @@
+public class zadanie_4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
+        int a = 7;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
