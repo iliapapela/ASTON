32f014b193364197fca00315729f5c3937efc39b
iliapapela <iliapapela@mail.ru>
2024-05-27 22:35:54 +0300

    6 done

---
+++ src/zadanie_6.java
@@ -0,0 +1,14 @@
+public class zadanie_6 {
+    public static void main(String[] args) {
+        verifyNumber(-7);
+
+    }
+    public static void verifyNumber(int number) {
+        if (number >=0) {
+            System.out.println("Число положительно");
+        }
+        else {
+            System.out.println("Число отрицательное");
+        }
+    }
+}
