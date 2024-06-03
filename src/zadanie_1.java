8468078ee99046c98d872f537a9c73ddef9bf64f
iliapapela <iliapapela@mail.ru>
2024-06-03 22:59:34 +0300

    1Done

--- src/zadanie_1.java
+++ src/zadanie_1.java
@@ -1,11 +1,33 @@
 public class zadanie_1 {
-    // задание 1
     public static void main(String[] args) {
-        printTreeWords () ;
+        Sotrudnik sotrudnik = new Sotrudnik("Фамилия Имя Отчество", "Ку А", "какаято_почта@mail.com", "123654852", 379000, 37);
+        sotrudnik.otput();
     }
-    public static void printTreeWords () {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+}
+
+class Sotrudnik {
+    private String FIO;
+    private String jobTitle;
+    private String email;
+    private String phone;
+    private double salary; // Используем тип double для зарплаты
+    private int age;       // Используем тип int для возраста
+
+    public Sotrudnik(String FIO, String jobTitle, String email, String phone, double salary, int age) {
+        this.FIO = FIO;
+        this.jobTitle = jobTitle;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void otput() {
+        System.out.println("ФИО: " + FIO);
+        System.out.println("Должность: " + jobTitle);
+        System.out.println("email: " + email);
+        System.out.println("телефон: " + phone);
+        System.out.println("зарплата: " + salary);
+        System.out.println("возраст: " + age);
     }
 }
