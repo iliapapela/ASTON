2b84651a467284736917042b43ae878139dc5ef4
iliapapela <61408447+iliapapela@users.noreply.github.com>
2024-06-03 22:51:27 +0300

    Delete src/zadanie_5.java

--- src/zadanie_5.java
+++
@@ -1,9 +0,0 @@
-public class zadanie_5 {
-    public static void main(String[] args) {
-        System.out.println(twoNumbers(7,3));
-    }
-    public static boolean twoNumbers(int num1, int num2) {
-        int sum = num1 + num2;
-        return sum >= 10 && sum <= 20;
-    }
-}
